eb73cdb15ce0798c1c5d983c1d492eb5d77eae59
raspberrypisig <raspberrypisig@gmail.com>
2018-05-17 13:17:37 +1000

    Update suncalc.c

--- suncalc.c
+++ suncalc.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+
 #define SECS_PER_MIN ((time_t)(60UL))
 #define SECS_PER_HOUR ((time_t)(3600UL))
 #define SECS_PER_DAY  ((time_t)(SECS_PER_HOUR * 24UL))
@@ -6,8 +8,14 @@
 #define LEAP_YEAR(Y) ( ((1970+(Y))>0) && !((1970+(Y))%4) && ( ((1970+(Y))%100) || !((1970+(Y))%400) ) )
 #define MELBOURNE_TIMEZONE 10
 #define MELBOURNE_TIMEZONE_DST 11
+#define DAYS_MS 86400000
+#define DAYS_MS_SMALL 86400
+#define J1970 2440588
+#define J2000 2451545
+#define PI 3.14
+#define RAD (180/PI)
+#define E (RAD * 23.4397)
 
-typedef long time_t;
 
 int monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
 
@@ -21,7 +29,7 @@ typedef struct  {
   int Year;   // offset from 1970; 
 } tmElements_t;
 
-time_t makeTime(tmElements_t tm)
+long long makeTime(tmElements_t tm)
 {   
 // assemble time elements into time_t 
 // note year argument is offset from 1970 (see macros in time.h to convert to other formats)
@@ -68,8 +76,6 @@ bool isDst(tmElements_t t) {
 
   int x = (year + year/4 + 5) % 7;
   int y = (year + year/4 + 6) % 7;
-  printf("%d\n", x);
-  printf("%d\n", y);
 
   if ( month == 4) {
     if ( day > (7 - x) ) {
@@ -101,10 +107,13 @@ bool isDst(tmElements_t t) {
 
 }
 
-/*
-def toMillis(date):
-    return (date - epochStart).total_seconds() * 1000
-*/
+float toJulian(time_t t) {
+  return (float)(t/DAYS_MS_SMALL) - 0.5 + J1970;
+}
+
+float toDays(time_t t) {
+    return toJulian(t) - J2000;
+}
 
 tmElements_t specify_time(int day, int month, int year, int hour, int minute, int second) {
     tmElements_t t;
@@ -121,10 +130,53 @@ tmElements_t specify_time(int day, int month, int year, int hour, int minute, in
     return t;
 }
 
+float siderealTime(float d, float lw) {
+    return RAD * (280.16 + 360.9856235 * d) - lw;
+}
+
+float rightAscension(float l, float b) {
+    return atan2(sin(l) * cos(E) - tan(b) * sin(E), cos(l));
+}
+
+float declination(float l, float b) {
+    return asin(sin(b) * cos(E) + cos(b) * sin(E) * sin(l));
+}
+
+float solarMeanAnomaly(float d) {
+    return RAD * (357.5291 + 0.98560028 * d);
+}
+
+float eclipticLongitude(float M) {
+    float C = RAD * (1.9148 * sin(M) + 0.02 * sin(2 * M) + 0.0003 * sin(3 * M));  
+    float P = RAD * 102.9372;
+    return M + C + P + PI;
+}
+
+void sunCoords(float d) {
+    float M = solarMeanAnomaly(d);
+    float L = eclipticLongitude(M);
+}
+
+void solarPosition(time_t date, float latitude, float longitude) {
+    float lw = RAD * -longitude;
+    float phi = RAD * latitude;
+    float d = toDays(date);
+    //c = sunCoords(d)
+    //H = siderealTime(d, lw) - c['ra']
+}
+
 int main()
 {
-    tmElements_t t = specify_time(16,5,2018,13,04,00);
+    tmElements_t t = specify_time(17,5,2018,12,47,00);
     time_t setTime = makeTime(t);
-    printf("%ld\n", setTime);
+    //printf("%d\n", sizeof(int));
+    //printf("%d\n", sizeof(long));
+    //printf("%d\n", sizeof(long long));
+    //printf("%d\n", sizeof(setTime));
+    //printf("%d\n", sizeof(time_t));
+
+    printf("%lu\n", setTime);
+    printf("%f\n", toJulian(setTime));
+    printf("%f\n", toDays(setTime));
     return 0;
 }
