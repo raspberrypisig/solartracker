48b6bf6178ce4655e87623ab6f1d67fc50d1f02d
raspberrypisig <raspberrypisig@gmail.com>
2018-05-31 18:28:14 +1000

    Add files via upload

--- GUI/main.py
+++ GUI/main.py
@@ -11,16 +11,31 @@ class StepperControllerHandler(object):
         self.builder = builder
         self.manager = StepperControllerManager()
         self.addMarksToSlider()
+
+        enterKey, mods = Gtk.accelerator_parse('Return')
+        self.enterKey = enterKey
+
+        leftKey, mods = Gtk.accelerator_parse('Left')
+        upKey, mods = Gtk.accelerator_parse('Up')
+        downKey, mods = Gtk.accelerator_parse('Down')
+        rightKey, mods = Gtk.accelerator_parse('Right')
+
+        self.arrowKeys = [leftKey, upKey, rightKey, downKey]
+        self.directions = {}
+        self.directions[leftKey] = 'left'
+        self.directions[upKey] = 'up'
+        self.directions[rightKey] = 'right'
+        self.directions[downKey] = 'down'
         # keycodes
         # https://gitlab.gnome.org/GNOME/gtk/raw/master/gdk/gdkkeysyms.h
-        key, mods = Gtk.accelerator_parse('u')
-        print(key, mods)
-        accelgroup = self.builder.get_object("mainaccel")
+        #key, mods = Gtk.accelerator_parse('u')
+        #print(key, mods)
+        #accelgroup = self.builder.get_object("mainaccel")
 
-        accelgroup.connect(
-            key,
-            mods,
-            Gtk.AccelFlags.VISIBLE, self.up)
+        # accelgroup.connect(
+        #    key,
+        #    mods,
+        #    Gtk.AccelFlags.VISIBLE, self.up)
         #accelmap = Gtk.AccelMap.get()
         # accelmap.change_entry(
         #    "<Actions>/mainaccel/up",
@@ -41,12 +56,26 @@ class StepperControllerHandler(object):
     def unitsChanged(self, widget, data=None):
         pass
 
-    def up(self, a, b, c, d):
-        print("up")
+    def arrowPressed(self, arrow):
+        print(arrow)
+        print(self.directions[arrow])
+        #self.move(None, None, arrow)
 
-    def move(self, widget, data=None):
+        # def up(self, a, b, c, d):
+        #    print("up")
+
+    def enterPressed(self, widget, data):
+        if data.keyval == self.enterKey:
+            print("enter pressed")
+            grid = self.builder.get_object("slider")
+            grid.grab_focus()
+            widget.grab_remove()
+        return False
+
+    def move(self, widget, data=None, direction=None):
         contstep = self.builder.get_object("contstep")
-        direction = Gtk.Buildable.get_name(widget)
+        if direction is None:
+            direction = Gtk.Buildable.get_name(widget)
         if contstep.get_active():
             self.manager.moveForever(direction)
         else:
@@ -76,6 +105,14 @@ class StepperControllerHandler(object):
     def reset(self, widget):
         slider = self.builder.get_object("slider")
         slider.set_value(650)
+
+    def keyPressed(self, widget, event):
+        if event.keyval in self.arrowKeys:
+            # print(Gtk.Buildable.get_name(widget))
+            # print(event.keyval)
+            self.arrowPressed(event.keyval)
+            return True
+        return False
 
 
 if __name__ == '__main__':
